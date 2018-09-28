40773e185583acaca6af88e2b1e9d52baf6ce75b
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-09-28 22:37:24 +0800

    add Feign模块

---
+++ feign-consumer/src/main/java/com/payne/study/service/HelloService.java
@@ -0,0 +1,19 @@
+package com.payne.study.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @program: spring-cloud-study
+ * @description:
+ * @author: Huizhe Yu
+ * @create: 2018-09-28 22:04
+ */
+@FeignClient(value = "eureka-client")
+public interface HelloService {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hiService();
+
+}
