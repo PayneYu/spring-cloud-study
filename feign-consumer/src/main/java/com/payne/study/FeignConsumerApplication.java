44084102d3d6ef4481640c980fe10e19a774be6e
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-09-29 08:47:08 +0800

    add Feign模块

---
+++ feign-consumer/src/main/java/com/payne/study/FeignConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.payne.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class FeignConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignConsumerApplication.class, args);
+	}
+}
