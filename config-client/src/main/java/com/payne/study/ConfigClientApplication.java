b68d73325d724a146473841305d8b9a5d475cb22
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-10-08 09:31:13 +0800

    添加spring cloud config

---
+++ config-client/src/main/java/com/payne/study/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.payne.study;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+	@Value("${name}")
+	private String name;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return "I am "+name;
+	}
+}
