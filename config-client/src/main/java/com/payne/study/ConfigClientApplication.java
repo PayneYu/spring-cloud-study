b44df75721964edba39c34912361d3957150b9ca
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-10-09 15:14:41 +0800

    添加spring cloud bus

--- config-client/src/main/java/com/payne/study/ConfigClientApplication.java
+++ config-client/src/main/java/com/payne/study/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
