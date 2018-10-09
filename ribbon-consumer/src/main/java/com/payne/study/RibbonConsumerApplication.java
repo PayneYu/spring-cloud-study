d3e68d14ba2cbabfff471044ead120467458eaef
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-10-09 18:51:10 +0800

    添加Hystrix Dashboard

--- ribbon-consumer/src/main/java/com/payne/study/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/payne/study/RibbonConsumerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class RibbonConsumerApplication {
 
 	public static void main(String[] args) {
