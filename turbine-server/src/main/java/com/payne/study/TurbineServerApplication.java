ee8d876566dfdeca573c38a99fbee9c6e129d4eb
huizhe.yu@thermofisher.com <huizhe.yu@thermofisher.com>
2018-10-10 11:55:54 +0800

    添加Hystrix turbine

---
+++ turbine-server/src/main/java/com/payne/study/TurbineServerApplication.java
@@ -0,0 +1,16 @@
+package com.payne.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbine
+public class TurbineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineServerApplication.class, args);
+	}
+}
